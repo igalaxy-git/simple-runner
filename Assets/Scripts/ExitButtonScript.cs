92401377abf857432d5a45308783c5506457340b
GALINA <igneous.galaxy003@gmail.com>
2020-12-09 00:04:20 +1000

    Exit Button

---
+++ Assets/Scripts/ExitButtonScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ExitButtonScript : MonoBehaviour
+{
+    void Start()
+    {
+        gameObject.GetComponent<Button>().onClick.AddListener(QuitGame);
+    }
+    
+    private void QuitGame()
+    {
+        Application.Quit();
+    }
+}
